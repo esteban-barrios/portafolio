a909a890d7e13682ed9cde561cc81eb415f0b2b7
esteban <esteban.barrios@sansano.usm.cl>
2025-02-13 13:12:23 -0300

    first layout

---
+++ src/lib/components/LinkButton.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  let {text} = $props();
+</script>
+
+<li class="link-button">
+  <a href="/"> {text} </a>
+</li>
+
+<style>
+  .link-button{
+    margin:2rem 0rem;
+    text-transform: uppercase;
+    font-size: 20px;
+  }
+  a { 
+    color:white;
+  }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import "$lib/global.css";
+  import SideBar from "$lib/components/SideBar.svelte";
+
+  let { children } = $props();
+</script>
+
+
+<SideBar/>
+<main>
+  {@render children()}
+</main>
+
+<style>
+ main {
+    position: fixed;
+    left: clamp(6.25rem + 4rem, 14% + 4rem, 16.25rem + 4rem);
+    width: 100%;  
+    width: -webkit-fill-available;
+    height: 100dvh;
+    padding: 2rem;
+    transition: all 0.3s ease-in-out;
+  }
+
+  /* Ajustes para mobile */
+  @media (max-width: 540px) {
+    main {
+      left: 0;
+      top: 4rem; /* Para quedar debajo de la barra de navegación */
+      width: 100%;  
+      width: -webkit-fill-available;
+      height: calc(100dvh - 4rem); /* Resta la altura de la barra */
+      padding: 1rem;
+    }
+  }
+</style>
